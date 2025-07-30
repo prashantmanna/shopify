fb90acbc112b971367a8a99bdcf0afa7199fe211
Prashant Manna <Prashant12@$>
2025-07-30 14:53:40 +0530

    initial commit

--- app/src/main/java/com/example/shopify/LoginScreen.kt
+++ app/src/main/java/com/example/shopify/LoginScreen.kt
@@ -1,12 +1,16 @@
 package com.example.shopify
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 open class LoginScreen : AppCompatActivity() {
+    lateinit var backBtn : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,22 @@ open class LoginScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        backBtn = findViewById(R.id.backBtn)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
+
+    fun gotoSignUp(view: View) {
+        val intent = Intent(this,RegisterScreen::class.java)
+        startActivity(intent)
+
+    }
+    fun gotoForgotPassword(view: View) {}
 }
--- app/src/main/java/com/example/shopify/RegisterScreen.kt
+++ app/src/main/java/com/example/shopify/RegisterScreen.kt
@@ -1,12 +1,15 @@
 package com.example.shopify
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterScreen : AppCompatActivity() {
+    lateinit var sign_in : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,12 @@ class RegisterScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        sign_in = findViewById(R.id.goToSignIn)
+        sign_in.setOnClickListener {
+            val intent = Intent(this,LoginScreen::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
